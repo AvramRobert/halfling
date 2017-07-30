a7d58ba8e6e09a745cf722600f629e81ea43edca
Robert Marius Avram <am11.robert@yahoo.com>
2017-07-30 14:22:36 +0200

    Release creds

--- project.clj
+++ project.clj
@@ -4,4 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/test.check "0.9.0"]])
+                 [org.clojure/test.check "0.9.0"]]
+  :repositories [["releases" {:url "https://clojars.org/repo"
+                              :creds :gpg}]])
